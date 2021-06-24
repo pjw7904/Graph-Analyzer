1aaa9374ed797d54ca25fbb8902bf3af44ade67a
pjw7904 <pjw7904@g.rit.edu>
2021-06-24 12:04:33 -0400

    Updates to DA logic and logging

--- PW_Scripts/DA.py
+++ PW_Scripts/DA.py
@@ -3,8 +3,14 @@ DIJKSTRA'S ALGORITHM
 ==========================='''
 from networkx import get_node_attributes
 
+# Location/name of the log file
+LOG_FILE = "DA_Output.txt"
+
 def DA(Graph, source):
     Graph.graph["DA"] = 0 # count number of iterations needed
+    Graph.graph["DA_recv"] = 0
+
+    logFile = open(LOG_FILE, "w")
 
     EDGE_COST = 1 # graphs are unweighted, all edges have a cost of 1
 
@@ -16,7 +22,7 @@ def DA(Graph, source):
             Graph.nodes[node]["dist"] = float('inf') # All other nodes in the graph are given the place-holder distance of infinity
             Graph.nodes[node]["parent"] = "udef"
 
-    Q = get_node_attributes(Graph, "dist")
+    Q = get_node_attributes(Graph, "dist") # unvisited set
 
     # while Q is not empty, meaning there are still more nodes that haven't been discovered/marked
     while Q:
@@ -25,19 +31,39 @@ def DA(Graph, source):
         # get the node with the lowest distance value, which starts with the root/source
         v = min(Q, key=Q.get)
 
+        logDAEvent("---------\n({0}) Visted Node: {1} | Distance: {2}\n".format(Graph.graph["DA"], v, Q[v]), logFile)    
+
+        # Now that the node has been "visted", it is deleted from the unvisited set
         del Q[v]
 
+        # For each neighbor of v, update the distance from the root if it is lower than the previous distance
         for u in Graph.neighbors(v):
+            neighborInfo = "\t{0} [distance: {1} | parent: {2}]: ".format(u, Graph.nodes[u]["dist"], Graph.nodes[u]["parent"])
+            
             if u in Q:
-                alt = Graph.nodes[v]["dist"] + EDGE_COST
+                alt = Graph.nodes[v]["dist"] + EDGE_COST # distance = distance of v + 1 (unweighted edge cost)
+                
                 if alt < Graph.nodes[u]["dist"]:
                     Graph.nodes[u]["dist"] = alt
-                    Graph.nodes[u]["parent"] = v
+                    Graph.nodes[u]["parent"] = v # parent node is now v, as that it how it gets back to root
                     Q[u] = alt
+                    neighborInfo += "distance ---> {0} | parent ---> {1}\n".format(alt, v)
+
+                    Graph.graph["DA_recv"] += 1
+                
+                else:
+                    neighborInfo += "no change, higher cost path\n"
+            else:
+                neighborInfo += "already visited\n"
+            
+            logDAEvent(neighborInfo, logFile)
 
 
+    # log the resulting SPT and its associated data (node distance + parent)
     result = getNodeInfo(Graph)
-    print(result)
+    logDAEvent("\n=====RESULT=====\n" + result, logFile)
+
+    logFile.close()
 
     return
 
@@ -49,3 +75,10 @@ def getNodeInfo(Graph):
         output += "{0}\n\tparent: {1}\n\tdistance from root: {2}\n".format(node, Graph.nodes[node]["parent"], Graph.nodes[node]["dist"])
     
     return output
+
+
+# Log a given DA event
+def logDAEvent(eventMsg, logFile):
+    logFile.write(eventMsg)
+
+    return
