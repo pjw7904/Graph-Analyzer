efe63ae27c437c3911a50427ce9176fcae9e87ad
Peter Willis <pjw7904@rit.edu>
2022-07-01 14:15:33 -0400

    update README

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # MTP-Analysis
 
 A collection of code and documentation for the purposes of defining and analyzing the properties of Meshed Trees.
+
+## Dependencies / Requirements
+
+- Python $\geq$ 3.8
+- ConfigParser
+- matplotlib
+- networkx
+- numpy
+- pydot
+- pygraphviz
+- tabulate
+
+## Running
+
+usage: graphanalyzer [-h] [--root vertex] [--remedy] [-m numOfBackups] [-r vertex1 vertex2] [-p] {mta,npaths,rsta,da,none}
+
+Graph and shortest path tree algorithm analysis script. Please update JSON config files before running.
+
+positional arguments:
+  {mta,npaths,rsta,da,none}
+                        tree algorithm to run on graph (or none).
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --root vertex         the root of the tree (leave blank for none)
+  --remedy              (MTA N-paths) m number of remedy paths, not any paths
+  -m numOfBackups       (MTA N-paths) number of backup paths, N = m+1
+  -r vertex1 vertex2    (MTA N-Paths) remove edge to test algorithm recovery
+  -p, --picture         Save Graphviz-generated picture of graph
--- graphanalyzer/parseConfig.py
+++ graphanalyzer/parseConfig.py
@@ -13,10 +13,10 @@ def parseArgs():
     argParser.add_argument('algorithm', choices=["mta", "npaths", "rsta", "da", "none"], help="tree algorithm to run on graph (or none).")
 
     # Tree algorithm modifiers
-    argParser.add_argument('--root', default=0, type=int, help="the root of the tree (leave blank for none)")
-    argParser.add_argument("--remedy", default=False, action="store_true", help="(MTA N-paths) N number of remedy paths, not any paths") # MTA N-paths, remedy paths
-    argParser.add_argument("-m", "--backups", default=2, type=int, help="(MTA N-paths) number of backup paths, N = m+1") # MTA N-paths, number of additional paths
-    argParser.add_argument("-r", "--remove", type=int, nargs=2, help="(all algorithms, only MTA N-paths confirmed to work) remove edge to test algorithm recovery") # Allow the user to remove an edge from the graph (result is algorithm-dependent)
+    argParser.add_argument('--root', default=0, type=int, metavar="vertex", help="the root of the tree (leave blank for none)")
+    argParser.add_argument("--remedy", default=False, action="store_true", help="(MTA N-paths) m number of remedy paths, not any paths") # MTA N-paths, remedy paths
+    argParser.add_argument("-m", "--backups", default=2, type=int, metavar='numOfBackups', help="(MTA N-paths) number of backup paths, N = m+1") # MTA N-paths, number of additional paths
+    argParser.add_argument("-r", "--remove", type=int, nargs=2, metavar=('vertex1', 'vertex2'), help="(all algorithms, only MTA N-paths confirmed to work) remove edge to test algorithm recovery") # Allow the user to remove an edge from the graph (result is algorithm-dependent)
 
     # Graph visualization
     argParser.add_argument("-p", "--picture", default=False, action="store_true", help="Save Graphviz-generated picture of graph") # Graphviz-based
