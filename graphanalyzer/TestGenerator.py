9c589ea4b7088e71b2d2832c413e91577c29eec1
Peter Willis <pjw7904@rit.edu>
2024-01-29 11:38:42 -0500

    Update graph naming pattern

--- graphanalyzer/TestGenerator.py
+++ graphanalyzer/TestGenerator.py
@@ -1,14 +1,13 @@
 ## Standard modules
-from os.path import join as getFile
 from pathlib import Path
 
-from networkx import NetworkXError, is_isomorphic, write_graphml
+from networkx import NetworkXError, write_graphml
 from networkx.utils import graphs_equal
 import GraphGenerator
 
 def generateRegularGraphs(numOfGraphs, numNodes, sharedDegree):
     graphsToSave = [] # Turn these into graphml files
-    graphLocation = f"C:/Users/peter/Documents/research/mtp-analysis/graphs/graphml/regular_{numNodes}_node"
+    graphLocation = f"C:/Users/peter/Documents/research/mtp-analysis/graphs/graphml/{sharedDegree}_regular_{numNodes}_node"
 
     Path(graphLocation).mkdir(parents=False, exist_ok=False)
 
@@ -31,7 +30,7 @@ def generateRegularGraphs(numOfGraphs, numNodes, sharedDegree):
                 else:
                     print(f"Saving graph #{graphNum+1}")
                     graphsToSave.append(candidateGraph)
-                    write_graphml(candidateGraph, graphLocation.format(f"regular_{numNodes}_{sharedDegree}_{graphNum+1}"))
+                    write_graphml(candidateGraph, graphLocation.format(f"regular_{numNodes}_{sharedDegree}_{graphNum+1}.graphml"))
                     break
             else:
                 currentAttempt += 1
